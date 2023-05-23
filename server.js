c152150174b5ef573a4ad6290ccefeb24b089c4f
Benjamin R. Zimmerman <bz931@berkeley.edu>
2023-05-23 11:12:24 -0700

    removed video files for now because i need to understand them first. added routing for index, topic pages, and 404 page + dynamic topic selection

--- server.js
+++ server.js
@@ -2,38 +2,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const server = require('http').Server(app);
-const io = require('socket.io')(server)
-const { v4: uuidv4  } = require('uuid');
-const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, {
-    debug: true
-});
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.static('assets'));
 
-app.use('/peerjs', peerServer);
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/index', (req, res) => {
+    const topics = ['Lobbying', 'Abortion', 'Gun Rights'];
+    res.render(path.join(__dirname, 'views/index.ejs'), {topics});
 });
 
-/*app.get('/', (req, res) => {
-    res.redirect(`/${uuidv4()}}`);
-}) Comment back in for hash generator*/
+app.get('/topic/:number', (req, res) => {
+    const n = req.params.number;
+    res.render(path.join(__dirname, 'views/topic.ejs'), {n});
+});
 
-// :room is parameter or variable for id. from redirect
-app.get('/:room', (req, res) => {
-    // render room file with unique room id. 
-    res.render('room', { roomId: req.params.room }) // req.params.room from res.redirect
-})
-
-io.on('connection', socket  => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        //socket.to(roomId).broadcast.emit('user-connected');
-        socket.to(roomId).emit('user-connected', userId);
-    })  
-})
+app.get('*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '/public/404.html'));
+});
 
 server.listen(3000, () => {
     console.log("listening on 3000");
