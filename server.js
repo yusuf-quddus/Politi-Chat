6bd4bdb48a5ec5c2c8392257e47b795ba8fd2d7c
Benjamin Riley Zimmerman <bz931@berkeley.edu>
2023-05-30 15:48:34 -0700

    started style sheet, syncing

--- server.js
+++ server.js
@@ -1,72 +1,73 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const server = require('http').Server(app);
+const express = require('express')
+const app = express()
+const path = require('path')
+const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuid } = require('uuid');
-const topics = ['Lobbying', 'Abortion', 'Gun Control'];
+const { v4: uuid } = require('uuid')
+const topics = ['Lobbying', 'Abortion', 'Gun Control']
 
-const Queue = require('./public/queue.js');
-const { get } = require('http');
-const lobbyingQueue = new Queue();
-const abortionQueue = new Queue();
-const gunControlQueue = new Queue();
+const Queue = require('./public/queue.js')
+const { get } = require('http')
+const lobbyingQueue = new Queue()
+const abortionQueue = new Queue()
+const gunControlQueue = new Queue()
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(express.static('assets'));
+app.use(express.static('public'))
+app.use(express.static('assets'))
+app.use(express.static('styles'))
 
 app.get('/index', (req, res) => {
-    res.render(path.join(__dirname, 'views/index.ejs'), { topics });
-});
+    res.render(path.join(__dirname, 'views/index.ejs'), { topics })
+})
 
 app.get('/topic/:t', (req, res) => {
-    const topic = req.params.t;
-    res.render(path.join(__dirname, 'views/topic.ejs'), { topic });
+    const topic = req.params.t
+    res.render(path.join(__dirname, 'views/topic.ejs'), { topic })
 });
 
 app.get('/topic/:t/redirect', (req, res) => {
-    const topic = req.params.t;
-    const opinion = req.query.opinion;
-    var q = getQueue(topic);
-    var roomId = q.pop();
+    const topic = req.params.t
+    const opinion = req.query.opinion
+    var q = getQueue(topic)
+    var roomId = q.pop()
     if (roomId == null) {
-        roomId = uuid();
+        roomId = uuid()
         q.push(roomId)
     }
-    res.redirect(`/topic/${topic}/${roomId}`);
-});
+    res.redirect(`/topic/${topic}/${roomId}`)
+})
 
 app.get('/topic/:t/:room', (req, res) => {
-    res.render(path.join(__dirname, 'views/room.ejs'), { roomId: req.params.room });
-});
+    res.render(path.join(__dirname, 'views/room.ejs'), { roomId: req.params.room })
+})
 
 app.get('*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, '/public/404.html'));
-});
+    res.status(404).sendFile(path.join(__dirname, '/public/404.html'))
+})
 
 // Contains protocol for handling connections
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.broadcast.to(roomId).emit('user-connected', userId);
-    });
-});
+        socket.join(roomId)
+        socket.broadcast.to(roomId).emit('user-connected', userId)
+    })
+})
 
 server.listen(3000, () => {
-    console.log("listening on 3000");
-});
+    console.log("listening on 3000")
+})
 
 function getQueue(topic) {
-    const ind = topics.indexOf(topic);
+    const ind = topics.indexOf(topic)
     switch(ind) {
         case -1:
-            return null;
+            return null
         case 0:
-            return lobbyingQueue;
+            return lobbyingQueue
         case 1:
-            return abortionQueue;
+            return abortionQueue
         case 2:
-            return gunControlQueue;
+            return gunControlQueue
     }
 }
