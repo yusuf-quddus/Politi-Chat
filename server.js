d6f15b640097952bbc36a5ab963ef2a305ad8f87
Yusuf Quddus <yusuf_quddus@yahoo.com>
2023-05-31 09:56:21 -0700

    restructure server.js

--- server.js
+++ server.js
@@ -11,8 +11,37 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use('/peerjs', peerServer);
+
+const topics = [
+    {
+        topic: 'Gun Control',
+        id: uuidv4(), 
+        description: "Should we have more or less gun control?",
+        image: "https://cdn.britannica.com/45/190745-050-1F7EC780/Gun-Constitution.jpg"
+    },
+    {
+        topic: 'Abortion',
+        id: uuidv4(),
+        description: "Should U.S law be pro-life or pro-choice?",
+        image: "https://www.marylandmatters.org/wp-content/uploads/2024/04/GettyImages-1458045593-scaled.jpg"
+    },
+    {
+        topic: 'Tax Policy',
+        id: uuidv4(),
+        description: "Should we increase or decrease taxes?",
+        image: "https://www.taxpolicycenter.org/sites/default/files/styles/landscape_480x360/public/initiatives/shutterstock_94126492_copy1_copy_0.jpg?itok=ekcY_b8I"
+    },
+    {
+        topic: 'Ukrain',
+        id: uuidv4(),
+        description: "Should we be more or less involved in the war in Ukraine?",
+        image: "https://www.globalr2p.org/wp-content/uploads/2023/03/Ukraine_64.png"
+    }
+]
+
 app.get('/', (req, res) => {
-    res.redirect(`/${uuidv4()}}`);
+    res.render('home', { topics })
+    //res.redirect(`/${uuidv4()}}`);
 })
 
 // :room is parameter or variable for id. from redirect
