bfb0eb8064f1764bec52653f1b8e8db90b2dd5f7
Yusuf Quddus <yusufquddus@berkeley.edu>
2023-06-14 17:38:53 -0700

    server testing

--- server.js
+++ server.js
@@ -15,34 +15,42 @@ app.use('/peerjs', peerServer);
 const topics = [
     {
         topic: 'Gun Control',
-        id: uuidv4(), 
+        room1: uuidv4(), 
+        room2: uuidv4(),
         description: "Should we have more or less gun control?",
         image: "https://cdn.britannica.com/45/190745-050-1F7EC780/Gun-Constitution.jpg"
     },
     {
         topic: 'Abortion',
-        id: uuidv4(),
+        room1: uuidv4(), 
+        room2: uuidv4(),
         description: "Should U.S law be pro-life or pro-choice?",
         image: "https://www.marylandmatters.org/wp-content/uploads/2024/04/GettyImages-1458045593-scaled.jpg"
     },
     {
         topic: 'Tax Policy',
-        id: uuidv4(),
+        room1: uuidv4(), 
+        room2: uuidv4(),
         description: "Should we increase or decrease taxes?",
         image: "https://www.taxpolicycenter.org/sites/default/files/styles/landscape_480x360/public/initiatives/shutterstock_94126492_copy1_copy_0.jpg?itok=ekcY_b8I"
     },
     {
         topic: 'Ukrain',
-        id: uuidv4(),
+        room1: uuidv4(), 
+        room2: uuidv4(),
         description: "Should we be more or less involved in the war in Ukraine?",
         image: "https://www.globalr2p.org/wp-content/uploads/2023/03/Ukraine_64.png"
     }
 ]
 
+var numClients = 0;
+
 app.get('/', (req, res) => {
-    res.render('home', { topics })
+    res.render('home', { topics, numClients })
+    console.log(numClients)
     //res.redirect(`/${uuidv4()}}`);
 })
+// trying to join room2 when numClients is in room 1 more than room 2
 
 // :room is parameter or variable for id. from redirect
 app.get('/:room', (req, res) => {
@@ -53,7 +61,7 @@ app.get('/:room', (req, res) => {
 io.on('connection', socket  => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
-        //socket.to(roomId).broadcast.emit('user-connected');
+        numClients = io.sockets.adapter.rooms.get(roomId).size
         socket.to(roomId).emit('user-connected', userId);
     })  
 })
