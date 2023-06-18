f26c08149ed5aa577d08def17c3e01b4b267368f
Yusuf Quddus <yusufquddus@berkeley.edu>
2023-06-18 13:54:36 -0700

    remove home.ejs, add more topics

--- server.js
+++ server.js
@@ -36,7 +36,32 @@ const topics = [
         topic: 'Ukraine',
         description: "Should we be more or less involved with Ukraine?",
         image: "https://www.globalr2p.org/wp-content/uploads/2023/03/Ukraine_64.png"
-    }
+    },
+    {
+        topic: 'Climate Change',
+        description: "Is climate change real?",
+        image: "https://cdn.mos.cms.futurecdn.net/xcLR5HMU2kxskdAy3ZVuTf.jpg"
+    },
+    {
+        topic: 'Covid-19 Lockdowns',
+        description: "Should the lockdowns have happened in response to the pandemic?",
+        image: "https://s3.amazonaws.com/cms.ipressroom.com/173/files/202211/638a99942cfac276d202c0d6_COVID+Image-Getty+Images/COVID+Image-Getty+Images_hero.jpg"
+    },
+    {
+        topic: 'Immigration',
+        description: "Should we have more or less immigration?",
+        image: "https://d1v9pyzt136u2g.cloudfront.net/blog/wp-content/uploads/2021/11/10114046/Immigration.png"
+    },
+    {
+        topic: 'Free Trade',
+        description: "Should we have free trade or protectionist policies?",
+        image: "https://cdn.britannica.com/56/194256-050-0DACD0D3/Concept-illustration-world-countries-trade-relationships.jpg"
+    },
+    {
+        topic: 'Aliens',
+        description: "Are aliens real? Have they made contact with earthlings?",
+        image: "https://dornsife.usc.edu/news/wp-content/uploads/sites/7/2023/04/story-3566-768x432.jpg"
+    },
 ]
 
 const qList = [
@@ -59,9 +84,37 @@ const qList = [
         topic: 'Ukraine',
         lowQueue: new Queue(),
         highQueue: new Queue()
-
-    }
+    },
+    {
+        topic: 'Climate Change',
+        lowQueue: new Queue(),
+        highQueue: new Queue()
+    },
+    {
+        topic: 'Covid-19 Lockdowns',
+        lowQueue: new Queue(),
+        highQueue: new Queue()
+    },
+    {
+        topic: 'Immigration',
+        lowQueue: new Queue(),
+        highQueue: new Queue()
+    },
+    {
+        topic: 'Free Trade',
+        lowQueue: new Queue(),
+        highQueue: new Queue()
+    },
+    {
+        topic: 'Affirmative Action',
+        lowQueue: new Queue(),
+        highQueue: new Queue()
+    },
 ]
+
+app.get('/', (req, res) => {
+    res.redirect('index')
+})
 
 app.get('/index', (req, res) => {
     res.render(path.join(__dirname, 'views/index.ejs'), { topics })
